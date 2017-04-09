b892d471d75f71062bd78f4f41bed01b4f0858df
deepu598 <meherdeepika598@gmail.com>
2017-04-09 15:47:20 +0530

    Update start.cpp

--- start.cpp
+++ start.cpp
@@ -1,1 +1,18 @@
-deepika
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+  int choice;
+  cin>>choice;
+
+   string Name;
+  getline(cin,Name);
+
+  cout<<"My name is "
+    <<Name
+<<"Choice "<<choice<<endl;
+  return 0;
+}
